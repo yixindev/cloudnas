697bdfa737e8515aff2bd98465d78e6703f677d1
yixin.jinhua <jinhua@mackbookm1>
2022-03-07 10:50:07 +0800

    update

---
+++ SampleCode/NASSDKDemo_iOS/Pods/NASSDK/nas_sdk/NASSDK.framework/Headers/NASSDK-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NASDefines.h"
+#import "NASSDK.h"
+
+FOUNDATION_EXPORT double NASSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char NASSDKVersionString[];
+
