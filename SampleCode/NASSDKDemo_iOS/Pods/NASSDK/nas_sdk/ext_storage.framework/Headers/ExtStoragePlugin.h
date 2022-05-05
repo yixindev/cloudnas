5b9d3227911b62829803e32324dd6bd4b6d61a9e
yixin.jinhua <jinhua@mackbookm1>
2022-05-05 11:08:29 +0800

    更新集成demo

--- SampleCode/NASSDKDemo_iOS/Pods/NASSDK/nas_sdk/ext_storage.framework/Headers/ExtStoragePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface ExtStoragePlugin : NSObject<FlutterPlugin>
-@end
--- SampleCode/NASSDKDemo_iOS/Pods/NASSDK/nas_sdk/pdf_render.framework/Headers/PdfRenderPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PdfRenderPlugin : NSObject<FlutterPlugin>
-@end
